d59677efb17916e4013c692635d347136e31c25c
Andranik Muradyan <muradianator@gmail.com>
2018-04-02 20:30:40 +0400

    Added JWT auth

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -4,6 +4,7 @@ import java.util
 import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.ws.TextMessage
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.{FromStringUnmarshaller, Unmarshaller}
@@ -13,6 +14,7 @@ import akka.stream.scaladsl.{Sink, Source}
 import com.google.gson.Gson
 import models.{Order, TickerData}
 import mt4commands.MT4Commands
+import pdi.jwt.{Jwt, JwtAlgorithm}
 import sources.TickerSource
 import specs.OrderSpec
 
@@ -39,6 +41,8 @@ object WebServer {
     implicit val executionCtx = actorSystem.dispatcher
 
     val password = "a72zkPP".toCharArray
+    val API_KEY = "280EC427-5184-4AB4-8A12-EF4140057437"
+    val secret_key = "I shot the sheriff"
 
     val ks: KeyStore = KeyStore.getInstance("PKCS12")
     val keystore: InputStream = getClass.getResourceAsStream("/cert.p12")
@@ -60,83 +64,104 @@ object WebServer {
       pathSingleSlash {
         complete("It's alive!!!")
       } ~
-      pathPrefix("exchanges") {
-        pathPrefix(Segment) {
-          exchangeId => {
-            pathPrefix("orders") {
-              pathEnd {
-                get {
-                  val ordersList = new util.ArrayList[Order]()
-                  MT4Commands(exchangeId).getOrders foreach ordersList.add
-
-                  complete(new Gson().toJson(ordersList))
-                }
-              } ~
-                post {
-                  entity(as[String]) {
-                    orderSpecJson => {
-                      val orderSpec = new Gson().fromJson(orderSpecJson, classOf[OrderSpec])
-                      complete(s"Order created for ${orderSpec.amount} of ${orderSpec.pair} in exchange $exchangeId")
-                    }
-                  }
-                }
-            } ~
-            pathPrefix("balance") {
-              pathEnd {
-                get {
-                  complete(MT4Commands(exchangeId).balance.toString)
-                }
-              } ~
-                path(IntNumber) {
-                  orderId => {
-                    pathEnd {
-                      delete {
-                        complete(MT4Commands(exchangeId).deleteOrder(orderId).toString)
-                      }
-                    }
-                  }
-                }
-            } ~
-            pathPrefix("tickers") {
-              path(Segment) {
-                ticker => {
-                  get {
-                    complete(s"Replying with data of ticker $ticker")
-                  }
-                }
-              } ~
-              get {
-                import CsvParameters._
-
-                parameters('tickers.as[List[String]].?) {
-                  tickers => {
-                    val tickerDataList = new util.ArrayList[TickerData]()
-                    tickers match {
-                      case Some(tickers: List[String]) => {
-                        MT4Commands(exchangeId).getTickerData(tickers) foreach tickerDataList.add
-                      }
-                      case None => {
-                        MT4Commands(exchangeId).getTickerData foreach tickerDataList.add
-                      }
-                    }
-                    complete(new Gson().toJson(tickerDataList))
-                  }
-                }
+      pathPrefix("login") {
+        pathEnd {
+          post {
+            entity(as[String])
+            { apiKey =>
+              {
+                if (apiKey.equals(API_KEY)) {
+                  val token = Jwt.encode(apiKey, secret_key, JwtAlgorithm.HS512)
+                  complete(token)
+                } else
+                complete(StatusCodes.InternalServerError)
               }
             }
-            path("ticker") {
-              extractUpgradeToWebSocket {
-                upgrade => {
-                  parameters('ticker.as[String]) {
-                    ticker => {
-                      complete({
-                        val tickerGraph = new TickerSource(exchangeId, ticker)
-                        val tickers = Source.fromGraph(tickerGraph).map(e => {
-                          TextMessage(e.toString)
-                        })
+          }
+        }
+      } ~
+      authorize(rc => {
+        val header = rc.request.getHeader("Authorization");
+        if(header.isPresent) Jwt.isValid(header.get().value(), secret_key, Seq(JwtAlgorithm.HS512)) else false
+      }) {
+        pathPrefix("exchanges") {
+          pathPrefix(Segment) {
+            exchangeId => {
+              pathPrefix("orders") {
+                pathEnd {
+                  get {
+                    val ordersList = new util.ArrayList[Order]()
+                    MT4Commands(exchangeId).getOrders foreach ordersList.add
 
-                        upgrade.handleMessagesWithSinkSource(Sink.ignore, tickers)
-                      })
+                    complete(new Gson().toJson(ordersList))
+                  }
+                } ~
+                  post {
+                    entity(as[String]) {
+                      orderSpecJson => {
+                        val orderSpec = new Gson().fromJson(orderSpecJson, classOf[OrderSpec])
+                        complete(s"Order created for ${orderSpec.amount} of ${orderSpec.pair} in exchange $exchangeId")
+                      }
+                    }
+                  }
+              } ~
+                pathPrefix("balance") {
+                  pathEnd {
+                    get {
+                      complete(MT4Commands(exchangeId).balance.toString)
+                    }
+                  } ~
+                    path(IntNumber) {
+                      orderId => {
+                        pathEnd {
+                          delete {
+                            complete(MT4Commands(exchangeId).deleteOrder(orderId).toString)
+                          }
+                        }
+                      }
+                    }
+                } ~
+                pathPrefix("tickers") {
+                  path(Segment) {
+                    ticker => {
+                      get {
+                        complete(s"Replying with data of ticker $ticker")
+                      }
+                    }
+                  } ~
+                    get {
+                      import CsvParameters._
+
+                      parameters('tickers.as[List[String]].?) {
+                        tickers => {
+                          val tickerDataList = new util.ArrayList[TickerData]()
+                          tickers match {
+                            case Some(tickers: List[String]) => {
+                              MT4Commands(exchangeId).getTickerData(tickers) foreach tickerDataList.add
+                            }
+                            case None => {
+                              MT4Commands(exchangeId).getTickerData foreach tickerDataList.add
+                            }
+                          }
+                          complete(new Gson().toJson(tickerDataList))
+                        }
+                      }
+                    }
+                } ~
+              path("ticker") {
+                extractUpgradeToWebSocket {
+                  upgrade => {
+                    parameters('ticker.as[String]) {
+                      ticker => {
+                        complete({
+                          val tickerGraph = new TickerSource(exchangeId, ticker)
+                          val tickers = Source.fromGraph(tickerGraph).map(e => {
+                            TextMessage(e.toString)
+                          })
+
+                          upgrade.handleMessagesWithSinkSource(Sink.ignore, tickers)
+                        })
+                      }
                     }
                   }
                 }
